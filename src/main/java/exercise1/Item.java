6bfef62b1e2987334945c64b9c91112d1dec954a
Paweł <p.kuklicz@gmail.com>
2018-01-06 15:07:17 +0100

    add counting discount to order.

--- src/main/java/exercise1/Item.java
+++ src/main/java/exercise1/Item.java
@@ -1,6 +1,14 @@
 package exercise1;
 
 class Item {
+    private static final int MEDIUM_SIZE = 10;
+    private static final int SMALL_SIZE = 5;
+    private static final int LARGE_SIZE = 20;
+    private static final double SMALL_DISCOUNT = 0.05;
+    private static final double MEDIUM_DISCOUNT = 0.1;
+    private static final double LARGE_DISCOUNT = 0.15;
+    private static final int NO_DISCOUNT = 0;
+
     private String itemName;
     private int quantity;
     private double price;
@@ -14,8 +22,32 @@ class Item {
     }
 
     double calculatePrice() {
-        return getQuantity() * getPrice();
+        return (getQuantity() * getPrice()) * (1 - calcTheValueWithDiscount());
     }
+
+    private double calcTheValueWithDiscount() {
+        double discount = NO_DISCOUNT;
+        if (getQuantity() >= SMALL_SIZE && getQuantity() < MEDIUM_SIZE) {
+            //discount  5%
+            discount = SMALL_DISCOUNT;
+        } else if (getQuantity() >= MEDIUM_SIZE && getQuantity() < LARGE_SIZE) {
+            //discount 10%
+            discount = MEDIUM_DISCOUNT;
+        } else if (getQuantity() >= LARGE_SIZE) {
+            //discount 15%
+            discount = LARGE_DISCOUNT;
+        }
+        return discount;
+    }
+
+    void show() {
+        System.out.printf("%-20s", getItemName());
+        System.out.printf("%-10s", (getPrice() + " PLN"));
+        System.out.printf("%4s", (getQuantity() + " pieces"));
+        System.out.printf("%10s", (calcTheValueWithDiscount() * 100 + "%"));
+        System.out.printf("%10s", (calculatePrice() + "PLN"));
+    }
+
 
     String getItemName() {
         return itemName;
@@ -25,28 +57,20 @@ class Item {
         this.itemName = itemName;
     }
 
-    void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     int getQuantity() {
         return quantity;
     }
 
-    void setPrice(double price) {
-        this.price = price;
+    void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     private double getPrice() {
         return price;
     }
 
-    void show() {
-        System.out.printf("%-20s", getItemName());
-        System.out.printf("%-10s", (getPrice() + " PLN"));
-        System.out.printf("%4s", (getQuantity() + " pieces"));
-        System.out.printf("%10s", (calculatePrice() + "PLN"));
+    void setPrice(double price) {
+        this.price = price;
     }
-
 
 }
